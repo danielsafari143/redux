f196b0080bd4b32217bbb5b3764facdf6f3167b3
danielsafari143 <danielsafari143@gmail.com>
2023-06-12 18:15:38 +0200

    add reducers

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createSlice , createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = "https://course-api.com/react-useReducer-cart-project"
 
 const initialState = {
-    cartItems : cartItems,
+    cartItems : [],
     amount : 4,
     total : 0,
     isLoading : false 
 };
+
+
+export const getCartItems = createAsyncThunk('cart/getCartItems' , async (thunkAPI) => {
+   try{
+    const reponse = await axios(url);
+    return reponse.data;
+   }catch{
+    return thunkAPI.rejectWithValue('something went wrong')
+   }
+}); 
 
 const cartSlice = createSlice({
     name:'cart',
@@ -37,6 +49,19 @@ const cartSlice = createSlice({
             });
             state.amount = amount;
             state.total = total
+        }
+    },
+    extraReducers : {
+        [getCartItems.pending] : (state) => {
+            state.isLoading = true;
+        },
+        [getCartItems.fulfilled] : (state , action) => {
+            state.isLoading = false;
+            state.cartItems = action.payload;
+        },
+        [getCartItems.rejected] : (state , action) => {
+            console.log(action.payload)
+            state.isLoading = false;
         }
     }
 });
